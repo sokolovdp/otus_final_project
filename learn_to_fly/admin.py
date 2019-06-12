de06606ec4d3f44c4c159e0fad221f59c73af69f
dmitrii <sokolovdp@gmail.com>
2019-06-12 16:23:27 +0300

    add userprofile model and admin

--- learn_to_fly/admin.py
+++ learn_to_fly/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from learn_to_fly.models import UserProfile
 
 # Register your models here.
+admin.site.register(UserProfile)
--- learn_to_fly/models.py
+++ learn_to_fly/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=None)
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+    category = models.CharField(max_length=40, null=True)
+
+    def __str__(self):
+        return self.user.username
