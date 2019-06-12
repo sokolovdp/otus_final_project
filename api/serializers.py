e36a4f72ec4dfc320a4b2252eed4d10eab6cb49f
dmitrii <sokolovdp@gmail.com>
2019-06-12 18:15:10 +0300

    add viewset for users api

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
+from django.contrib.auth.models import User
 from learn_to_fly.models import UserProfile
 
 
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
 class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
 
     class Meta:
         model = UserProfile
