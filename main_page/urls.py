cc25e484ea1149b5a7d42af90d196c929f298127
dmitrii <sokolovdp@gmail.com>
2019-06-16 09:59:10 +0300

    finalize course views

--- main_page/urls.py
+++ main_page/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('login/', views.user_login, name='user_login'),
     path('logout/', views.user_logout, name='user_logout'),
     path('courses_list/', views.courses_list, name='courses_list'),
-    path('course_detail/', views.course_detail, name='course_detail'),
+    path('course_detail/<int:pk>', views.course_detail, name='course_detail'),
 ]
