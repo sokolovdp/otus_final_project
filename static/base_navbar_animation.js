ec9d352223661f0861d0c05a8d636a5afd36b616
dmitrii <sokolovdp@gmail.com>
2019-06-29 14:49:53 +0300

    add js to show latin proverbs in random order each time the main page is reloaded

---
+++ static/base_navbar_animation.js
@@ -0,0 +1,26 @@
+
+function randomInt(range) {
+    return Math.floor((Math.random() * range));
+}
+
+function getJsonData(idName) {
+    let jsonData = document.getElementById(idName).innerHTML;
+
+    return JSON.parse(jsonData)
+}
+
+function replacePhrase(text) {
+    let header = document.querySelector(".phrase");
+
+    header.textContent = text;
+}
+
+function UpdateNavBar() {
+    let contentArray = getJsonData("contentJson");
+    let textIndex = randomInt(contentArray.length);
+    let phraseOfDay = contentArray[textIndex];
+
+    replacePhrase(phraseOfDay);
+}
+
+UpdateNavBar();
