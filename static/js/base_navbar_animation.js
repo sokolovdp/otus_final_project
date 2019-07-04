0a5a6c8f90b7adbdf70e9845f5959fdb91e472d1
dmitrii <sokolovdp@gmail.com>
2019-07-04 13:57:46 +0300

    remove calendar js and css

--- static/js/base_navbar_animation.js
+++ static/js/base_navbar_animation.js
@@ -3,12 +3,6 @@ function randomInt(range) {
     return Math.floor((Math.random() * range));
 }
 
-// function getJsonData(idName) {
-//     let jsonData = document.getElementById(idName).innerHTML;
-//
-//     return JSON.parse(jsonData)
-// }
-
 function replacePhrase(text) {
     let header = document.querySelector(".phrase");
 
@@ -16,7 +10,6 @@ function replacePhrase(text) {
 }
 
 function UpdateNavBar() {
-    // let contentArray = getJsonData("contentJson");
     let textIndex = randomInt(contentArray.length);
     let phraseOfDay = contentArray[textIndex];
 
