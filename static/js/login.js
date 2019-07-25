f028fdfca06491cbbe81fcd5611a574fbc0d5a9f
dmitrii <sokolovdp@gmail.com>
2019-07-25 13:43:28 +0300

    correct js style

--- static/js/login.js
+++ static/js/login.js
@@ -2,17 +2,15 @@
     Process login form: call login API, and in case of success redirect to main page
 */
 
-var ApiToken = '';
-
 function successFunction(xhttp) {
     let jsonString = xhttp.responseText;
-    let message = 'You have logged in, API token='
-    let json_data = JSON.parse(jsonString)
+    let message = 'You have logged in, API token=';
+    let json_data = JSON.parse(jsonString);
 
-    ApiToken = json_data.token
-    sessionStorage.setItem('apiToken', ApiToken);
+    let apiToken = json_data.token;
+    sessionStorage.setItem('apiToken', apiToken);
 
-    document.getElementById("login-result").innerHTML = message + ApiToken;
+    document.getElementById("login-result").innerHTML = message + apiToken;
     document.getElementById("login-result").className = 'alert alert-success';
 
     window.location.href = "../../templates/js/calendar.html";
@@ -30,10 +28,10 @@ function sendAuthPostRequest(url, jsonString) {
     let xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             successFunction(this);
         }
-        if (this.readyState == 4 && this.status != 200) {
+        if (this.readyState === 4 && this.status !== 200) {
             failFunction(this);
         }
     };
@@ -41,7 +39,6 @@ function sendAuthPostRequest(url, jsonString) {
     xhttp.open("POST", url, true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send(jsonString);
-
 }
 
 
@@ -53,7 +50,7 @@ $('#loginForm').on('submit', function (event) {
     });
     let jsonString = JSON.stringify(values);
 
-    sendAuthPostRequest('/api/v1/get_auth_token', jsonString)
+    sendAuthPostRequest('/api/v1/get_auth_token', jsonString);
 });
 
 
