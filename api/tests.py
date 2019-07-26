8aaf90b3a0be6434077bcf999e37d73b7ae5aadf
dmitrii <sokolovdp@gmail.com>
2019-07-26 15:28:58 +0300

    add test cases for api

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ApiTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_user_profile_viewset(self):
+        self.assertEqual(1, 1)
+
+    def test_course_viewset(self):
+        self.assertEqual(1, 1)
+
+    def test_student_course_registration_viewset(self):
+        self.assertEqual(1, 1)
+
+    def test_month_calendar_view(self):
+        self.assertEqual(1, 1)
