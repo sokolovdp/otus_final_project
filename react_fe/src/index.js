218bf9c2bd574990a4d700f9e6213661a8a29c6a
dp.sokolov <dp.sokolov@jet.su>
2019-08-13 17:34:18 +0300

    add routing and notfound

--- react_fe/src/index.js
+++ react_fe/src/index.js
@@ -7,6 +7,7 @@ import Main from './Main';
 // import Register from './Register';
 // import Login from './Login';
 // import Calendar from './calendar'
+import Notfound from './notfound'
 
 import 'bootstrap/dist/css/bootstrap.css';
 
