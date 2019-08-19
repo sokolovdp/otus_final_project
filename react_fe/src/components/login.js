a51607fa4edcbdb279f4b02ba4b0b4fe141b0145
dp.sokolov <dp.sokolov@jet.su>
2019-08-19 19:15:30 +0300

    update registration form

--- react_fe/src/components/login.js
+++ react_fe/src/components/login.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import MainHeader from './main_header'
 import PageTitle from './page_title'
-import PageContent from './page_content'
 import LoginForm from "../login_form";
 
 
