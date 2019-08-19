62fbb59df060315020964b3227f250a8586b57e4
dp.sokolov <dp.sokolov@jet.su>
2019-08-19 19:14:40 +0300

    update registration form

--- react_fe/src/components/registration_form.js
+++ react_fe/src/components/registration_form.js
@@ -6,51 +6,39 @@ export default class RegistrationForm extends React.Component {
     return (
       <Form>
         <Row form>
-          <Col md={6}>
+          <Col md={4}>
             <FormGroup>
-              <Label for="exampleEmail">Email</Label>
-              <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-            </FormGroup>
-          </Col>
-          <Col md={6}>
-            <FormGroup>
-              <Label for="examplePassword">Password</Label>
-              <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-            </FormGroup>
-          </Col>
-        </Row>
-        <FormGroup>
-          <Label for="exampleAddress">Address</Label>
-          <Input type="text" name="address" id="exampleAddress" placeholder="1234 Main St"/>
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleAddress2">Address 2</Label>
-          <Input type="text" name="address2" id="exampleAddress2" placeholder="Apartment, studio, or floor"/>
-        </FormGroup>
-        <Row form>
-          <Col md={6}>
-            <FormGroup>
-              <Label for="exampleCity">City</Label>
-              <Input type="text" name="city" id="exampleCity"/>
+              <Label for="formEmail">Email</Label>
+              <Input type="email" name="email" id="formEmail" placeholder="your email" />
             </FormGroup>
           </Col>
           <Col md={4}>
             <FormGroup>
-              <Label for="exampleState">State</Label>
-              <Input type="text" name="state" id="exampleState"/>
-            </FormGroup>
-          </Col>
-          <Col md={2}>
-            <FormGroup>
-              <Label for="exampleZip">Zip</Label>
-              <Input type="text" name="zip" id="exampleZip"/>
+              <Label for="formPassword">Password</Label>
+              <Input type="password" name="password" id="formPassword" placeholder="new password" />
             </FormGroup>
           </Col>
         </Row>
-        <FormGroup check>
-          <Input type="checkbox" name="check" id="exampleCheck"/>
-          <Label for="exampleCheck" check>Check me out</Label>
-        </FormGroup>
+        <Row form>
+          <Col md={4}>
+            <FormGroup>
+              <Label for="formUsername">Username</Label>
+              <Input type="text" name="city" id="formUsername"/>
+            </FormGroup>
+          </Col>
+          <Col md={4}>
+            <FormGroup>
+              <Label for="formFirstName">First Name</Label>
+              <Input type="text" name="state" id="fromFirstName"/>
+            </FormGroup>
+          </Col>
+          <Col md={4}>
+            <FormGroup>
+              <Label for="formSurname">Surname</Label>
+              <Input type="text" name="zip" id="formSurname"/>
+            </FormGroup>
+          </Col>
+        </Row>
         <Button>Sign up</Button>
       </Form>
     );
